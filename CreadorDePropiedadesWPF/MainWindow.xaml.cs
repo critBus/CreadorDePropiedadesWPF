843adf615e5a696e3c4eaa039e673e68a71c6b0a
Rene Lazaro Collado Arteaga <rene_collado@inca.edu.cu>
2023-02-23 15:30:53 -0500

    real
    2-23-2023

---
+++ CreadorDePropiedadesWPF/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReneUtiles;
+using ReneUtiles.Clases;
+using ReneUtiles.Clases.WPF;
+using ReneCreadorPropiedadesWPF;
+
+using ReneUtiles.Clases.BD;
+
+namespace CreadorDePropiedadesWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        private TemporalStorageBD TS;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TS = new TemporalStorageBD();
+            cargarEA();
+
+
+        }
+
+        private void cargarEA() {
+            setTextoSalida(TS.getStr("salida", ""));
+            setTextoEntrada(TS.getStr("entrada", ""));
+            seleccionarCopiar(TS.getBool("copiar",true)??true);
+            seleccionarPegar(TS.getBool("pegar", true) ?? true);
+            seleccionar_LasEntradasSonProperties(TS.getBool("entradasProperties", false) ?? false);
+        }
+        private void guardarEA()
+        {
+            TS.put("salida", getTextoSalida());
+            TS.put("entrada", getTextoEntrada());
+            TS.put("copiar", estaSeleccionadoCopiar());
+            TS.put("pegar", estaSeleccionadoPegar());
+            TS.put("entradasProperties", estaSeleccionado_LasEntradasSonProperties());
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+
+        private void btnMinimizeLogin_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCloseLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+
+        public void setTextoEntrada(string a)
+        {
+            this.setText(TB_Entrada, a);
+        }
+        public string getTextoEntrada()
+        {
+            return UtilesWPF.getText(TB_Entrada);// T_entrada.Text.Replace("\r","");
+        }
+        public void setTextoSalida(string a)
+        {
+            //	T_Salida.Text=a;
+            this.setText(TB_Salida, a);
+        }
+        public string getTextoSalida()
+        {
+            return this.getText(TB_Salida);// T_entrada.Text.Replace("\r","");
+        }
+
+        public bool estaSeleccionadoCopiar()
+        {
+            //return CB_PegarYAplicar.IsChecked==true;
+            return this.isSelect(CB_Copiar);
+        }
+        public bool estaSeleccionadoPegar()
+        {
+            //return CB_PegarYAplicar.IsChecked==true;
+            return this.isSelect(CB_Pegar);
+        }
+        public void seleccionarCopiar(bool seleccionar) {
+            this.setSelect(CB_Copiar, seleccionar); 
+        }
+        public void seleccionarPegar(bool seleccionar)
+        {
+            this.setSelect(CB_Pegar, seleccionar);
+        }
+
+        public bool estaSeleccionado_LasEntradasSonProperties()
+        {
+            //return CB_PegarYAplicar.IsChecked==true;
+            return this.isSelect(CB_LasEntradasSonProperties);
+        }
+        public void seleccionar_LasEntradasSonProperties(bool seleccionar)
+        {
+            this.setSelect(CB_LasEntradasSonProperties, seleccionar);
+        }
+
+        private void paste() {
+            setTextoEntrada(this.getTextoDeW());
+        }
+        private void copy()
+        {
+            this.copiarW(getTextoSalida());
+        }
+
+        public void crearCodigo(Func<string, string> getTextoEditado) {
+            if (estaSeleccionadoPegar())
+            {
+                paste();
+            }
+            string entrada = getTextoEntrada();
+            entrada = getTextoEditado(entrada);
+            setTextoSalida(entrada);
+            if (estaSeleccionadoCopiar())
+            {
+                copy();
+            }
+        }
+
+        //private void button_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void button_Click_1(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        private void alApretarBotonGeneral(object sender, RoutedEventArgs e)
+        {
+            crearCodigo(v=>new CreadorPropiedadesWPF(v, estaSeleccionado_LasEntradasSonProperties()).generarCodigo_Properties_Atributos());
+        }
+
+        private void AlCerrarLaVentana(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            guardarEA();
+            
+        }
+
+        private void alApretarBotonClear(object sender, RoutedEventArgs e)
+        {
+            setTextoEntrada("");
+            setTextoSalida("");
+        }
+
+        private void alApretarBotonPegar(object sender, RoutedEventArgs e)
+        {
+            paste();
+        }
+
+        private void alApretarBotonCopiar(object sender, RoutedEventArgs e)
+        {
+            copy();
+        }
+
+        private void apreto_Boton_Gets(object sender, RoutedEventArgs e)
+        {
+            crearCodigo(v => new CreadorPropiedadesWPF(v).generalCodigo_GetMetodos());
+        }
+
+        private void alApretar_ToString_Python(object sender, RoutedEventArgs e)
+        {
+            crearCodigo(v => new CreadorPropiedadesWPF(v).generalCodigo_ToStringPython());
+        }
+
+        private void apreto_Boton_Gets_Propiedades(object sender, RoutedEventArgs e)
+        {
+            crearCodigo(v => new CreadorPropiedadesWPF(v).generalCodigo_GetProperties());
+        }
+    }
+}
---
+++ CreadorDePropiedadesWPF/ReneCreadorPropiedadesWPF/CreadorDePropiedadesWPF.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReneUtiles;
+using ReneUtiles.Clases;
+
+namespace ReneCreadorPropiedadesWPF
+{
+    public class CreadorPropiedadesWPF:ConsolaBasica
+    {
+        private List<string> entradas;
+        private List<string> tipos;
+        private bool lasEntradasSonProperties;
+        //private bool capitalizeProperties;
+        //private bool pisoAtributos;
+        public CreadorPropiedadesWPF(string texto
+            , bool lasEntradasSonProperties = false
+            //, bool capitalizeProperties = true
+            //,bool pisoAtributos=true
+            ) {
+            //{ get; set; }
+            //
+            string[] paresEntradas= split(texto, ',', '\n');
+            this.tipos = new List<string>();
+            this.entradas = new List<string>();
+            for (int i = 0; i < paresEntradas.Length; i++)
+            {
+
+                List<string> separaciones = split(paresEntradas[i], ' ').ToList();
+                separaciones.RemoveAll(v =>or(v, "{", "get;", "set;","}"));
+                if (separaciones.Count()>0) {
+                    //entradas[i] = separaciones[separaciones.Count() - 1];
+                    entradas.Add(separaciones.Last().Replace(";", ""));
+                    if (separaciones.Count() > 1)
+                    {
+                        tipos.Add(separaciones[separaciones.Count() - 2]);
+                        
+                    }
+                    else
+                    {
+                        tipos.Add("string");
+                    }
+                }
+                
+            }
+            this.lasEntradasSonProperties = lasEntradasSonProperties;
+            //this.capitalizeProperties = capitalizeProperties;
+            //this.pisoAtributos = pisoAtributos;
+        }
+        public string generarCodigo_Properties_Atributos() {
+            Func<string, string> getNombreTratado = v => Utiles.eliminarAlInicio(v.Trim(), '_');
+            Func<string, string> getNombrePropertie=null;
+
+            if (this.lasEntradasSonProperties)
+            {
+                //getNombrePropertie = v => this.capitalizeProperties ? Utiles.capitalize(getNombreTratado(v)) : getNombreTratado(v);
+                getNombrePropertie = v =>  getNombreTratado(v);
+
+            }
+            else
+            {
+                getNombrePropertie = v => Utiles.capitalize(getNombreTratado(v));
+            }
+
+            Func<string, string> getNombreAtributo = null;
+
+
+            getNombreAtributo = v => "_"+Utiles.primerCharMinuscula(getNombreTratado(v));
+
+            int iden = 0;
+            string r = "";
+
+            for (int i = 0; i < this.entradas.Count(); i++)
+            {
+                r += Utiles.identacionLn(iden,1)+"private "+this.tipos[i]+" "+getNombreAtributo(this.entradas[i])+";";
+            }
+
+            for (int i = 0; i < this.entradas.Count(); i++)
+            {
+                r += Utiles.identacionLn(iden, 1) + "public " + this.tipos[i] + " " + getNombrePropertie(this.entradas[i]) ;
+                r += Utiles.identacionLn(iden, 1) + "{";
+                r += Utiles.identacionLn(iden, 2) + "get";
+                r += Utiles.identacionLn(iden, 2) + "{";
+                r += Utiles.identacionLn(iden, 3) + "return "+ getNombreAtributo(this.entradas[i]) + ";";
+                r += Utiles.identacionLn(iden, 2) + "}";
+//                r += Utiles.identacionLn(iden, 1) + "{";
+                r += Utiles.identacionLn(iden, 2) + "set";
+                r += Utiles.identacionLn(iden, 2) + "{";
+                r += Utiles.identacionLn(iden, 3) +  getNombreAtributo(this.entradas[i]) + " = value;";
+                r += Utiles.identacionLn(iden, 3) + "OnPropertyChanged(nameof("+ getNombrePropertie(this.entradas[i]) + "));";
+                r += Utiles.identacionLn(iden, 2) + "}";
+                r += Utiles.identacionLn(iden, 1) + "}";
+            }
+
+            return r;
+        }
+
+        public string generalCodigo_GetMetodos() {
+            Func<string, string> getNombreMetodo = v => "Get" + Utiles.primerCharMayuscula(v);
+            int iden = 0;
+            string r = "";
+            for (int i = 0; i < this.entradas.Count(); i++)
+            {
+                string atributo = this.entradas[i];
+                string tipo = this.tipos[i];
+                r += Utiles.identacionLn(iden, 1) + "public " + tipo + " " + getNombreMetodo(atributo)+"()";
+                r += Utiles.identacionLn(iden, 1) + "{";
+                r += Utiles.identacionLn(iden, 2) + "return this." + atributo + ";";
+                r += Utiles.identacionLn(iden, 1) + "}";
+            }
+            return r;
+        }
+
+
+        public string generalCodigo_GetProperties()
+        {
+            Func<string, string> getNombrePropertie = v =>  Utiles.primerCharMayuscula(v);
+            int iden = 0;
+            string r = "";
+            for (int i = 0; i < this.entradas.Count(); i++)
+            {
+                string atributo = this.entradas[i];
+                string tipo = this.tipos[i];
+                //r += Utiles.identacionLn(iden, 1) + "public " + tipo + " " + getNombreMetodo(atributo) + "()";
+                //r += Utiles.identacionLn(iden, 1) + "{";
+                //r += Utiles.identacionLn(iden, 2) + "return this." + atributo + ";";
+                //r += Utiles.identacionLn(iden, 1) + "}";
+
+
+                r += Utiles.identacionLn(iden, 1) + "public " + tipo + " " + getNombrePropertie(atributo);
+                r += Utiles.identacionLn(iden, 1) + "{";
+                r += Utiles.identacionLn(iden, 2) + "get";
+                r += Utiles.identacionLn(iden, 2) + "{";
+                r += Utiles.identacionLn(iden, 3) + "return this." + atributo + ";";
+                r += Utiles.identacionLn(iden, 2) + "}";
+               
+                
+                r += Utiles.identacionLn(iden, 1) + "}";
+            }
+            return r;
+        }
+
+        public string generalCodigo_ToStringPython()
+        {
+            int iden = 0;
+            string separacion1 = Utiles.identacionLn(iden, 1);
+            string separacion2 = Utiles.identacionLn(iden, 2);
+            string separacion3 = Utiles.identacionLn(iden, 3);
+
+            string mr = "";
+            mr += separacion1 + "def __str__(self):";
+            mr += separacion2 + "return self.__class__.__name__";
+            mr += this.entradas.Count()>0 ? "\\" : "";
+            //string separacion3 = getSeparacionln(3,separacion0);
+            for (int i = 0; i < this.entradas.Count(); i++)
+            {
+                string atributo = this.entradas[i];
+                if (atributo.Contains(":")) {
+                    atributo = subs(atributo, 0, atributo.IndexOf(':'));
+                }
+                
+                mr += separacion3 + "+\" " + atributo + "=\"+str(self." + atributo+")";
+                mr += i != this.entradas.Count() - 1?"\\":"";
+            }
+            
+
+            return mr;
+        }
+
+
+
+
+        }
+}
